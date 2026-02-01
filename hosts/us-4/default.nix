4cf17c4ae52ed3effacd259096b00fe4db3c5293
Hakula Chen <i@hakula.xyz>
2026-02-01 21:55:47 +0800

    feat(hosts/us-4): enable all services on us-4

--- hosts/us-4/default.nix
+++ hosts/us-4/default.nix
@@ -27,12 +27,53 @@ in
   hakula.builders.enable = true;
 
   # ============================================================================
+  # Credentials
+  # ============================================================================
+  hakula.cachix.enable = true;
+  hakula.claude-code.enable = true;
+  hakula.dockerHub = {
+    username = "hakula139";
+    tokenAgeFile = ../../secrets/shared/dockerhub-token.age;
+  };
+  hakula.mcp.enable = true;
+
+  # ============================================================================
   # Services
   # ============================================================================
+  hakula.services.aria2.enable = true;
+  hakula.services.backup = {
+    enable = true;
+    b2Bucket = "hakula-backup";
+    backupPath = hostName;
+    cloudreve.enable = true;
+    twikoo.enable = true;
+  };
+  hakula.services.clashGenerator.enable = true;
+  hakula.services.cloudreve = {
+    enable = true;
+    umami = {
+      enable = true;
+      workerHost = "b2.hakula.xyz";
+    };
+  };
+  hakula.services.piclist.enable = true;
+  hakula.services.netdata.enable = true;
+  hakula.services.nginx.enable = true;
   hakula.services.openssh = {
     enable = true;
     ports = [ 35060 ];
   };
+  hakula.services.postgresql.enable = true;
+  hakula.services.umami.enable = true;
+  hakula.services.xray = {
+    enable = true;
+    ws.enable = true;
+  };
+
+  # ============================================================================
+  # Home Manager Modules
+  # ============================================================================
+  home-manager.users.hakula.hakula.claude-code.enable = true;
 
   # ============================================================================
   # System State
