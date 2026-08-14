083d40a19008414424940ba39590deca1a6e908c
Hakula Chen <i@hakula.xyz>
2026-08-14 15:40:42 +0800

    docs(tooling): drop the comments that restate a package name

--- lib/tooling.nix
+++ lib/tooling.nix
@@ -9,24 +9,24 @@
   # Nix Development
   # ----------------------------------------------------------------------------
   nix = with pkgs; [
-    cachix # Cachix client (binary cache)
-    colmena # Multi-host NixOS deployment tool
-    deadnix # Find unused Nix bindings / attributes
-    nh # Nix CLI wrapper with nom / nvd integration
-    nixd # Nix language server (LSP)
-    nix-tree # Explore dependency tree of Nix derivations
-    nixfmt # Nix formatter
-    nom # nix-output-monitor (pretty build output)
-    nvd # Nix / NixOS diff tool (generations / closures)
-    statix # Nix linter
+    cachix
+    colmena
+    deadnix
+    nh
+    nixd
+    nix-tree
+    nixfmt
+    nom
+    nvd
+    statix
   ];
 
   # ----------------------------------------------------------------------------
   # Secrets Management
   # ----------------------------------------------------------------------------
   secrets = with pkgs; [
-    age # File encryption tool used by agenix
-    agenix # Manage age-encrypted secrets (*.age)
+    age
+    agenix
   ];
 
   # ----------------------------------------------------------------------------
